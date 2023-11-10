1b4cbf703db1b797ae6debf78bda97d958122b07
laosarah <139395413+laosarah@users.noreply.github.com>
2023-11-10 14:34:44 -0300

    T005: feat - implementação do componente movie-container

---
+++ movie-challenge/src/app/components/pages/home/movie-container/movie-container.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-container',
+  templateUrl: './movie-container.component.html',
+  styleUrls: ['./movie-container.component.css']
+})
+
+export class MovieContainerComponent {
+  @Input() movies: any[] = [];
+
+  constructor(
+  ){}
+}
