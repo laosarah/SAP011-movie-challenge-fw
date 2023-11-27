f2cf0f408d03335ce319b4a40a01477b9f446f16
laosarah <139395413+laosarah@users.noreply.github.com>
2023-11-27 11:44:45 -0300

    T010: feat - implementação da solicitação e ordenação da lista de generos

--- movie-challenge/src/app/services/tmdb/tmdb.service.spec.ts
+++ movie-challenge/src/app/services/tmdb/tmdb.service.spec.ts
@@ -1,13 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TmdbService } from './tmdb.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('TmdbService', () => {
   let service: TmdbService;
+  let httpTest: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        TmdbService
+      ]
+    });
     service = TestBed.inject(TmdbService);
+    httpTest = TestBed.inject(HttpTestingController)
   });
 
   it('should be created', () => {
